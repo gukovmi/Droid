99fbdcb7c5089c0c1d6059007ddc49a9e2b611ba
Maxim Gukov <gukovmi96@yandex.ru>
2021-03-20 20:30:09 +0700

    Changed base url

--- app/src/main/java/com/example/droid/loan/data/network/ValueStore.kt
+++ app/src/main/java/com/example/droid/loan/data/network/ValueStore.kt
@@ -1,5 +1,5 @@
 package com.example.droid.loan.data.network
 
 object ValueStore {
-    val BASE_URL = "http://focusapp-env.eba-xm2atk2z.eu-central-1.elasticbeanstalk.com/"
+    val BASE_URL = "https://droid-project.herokuapp.com/"
 }
